5d6f871835d591f2cd1c0df40693668761b74a7b
Shen Jun Wu <shenfive@gmail.com>
2021-01-09 14:50:42 +0800

    Initial Commit

---
+++ a211helloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  a211helloSwiftUI
+//
+//  Created by 申潤五 on 2021/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a211helloSwiftUI/a211helloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  a211helloSwiftUIApp.swift
+//  a211helloSwiftUI
+//
+//  Created by 申潤五 on 2021/1/9.
+//
+
+import SwiftUI
+
+@main
+struct a211helloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
