c6a244e9c1b1e383f49ccff897ac3c8c2c54cb74
Shen Jun Wu <shenfive@gmail.com>
2021-01-09 16:02:15 +0800

    MyView

--- a211helloSwiftUI/ContentView.swift
+++ a211helloSwiftUI/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+//import UIKit
 
 struct ContentView: View {
     @State var inputString:String = ""
     var body: some View {
         ZStack{
-            Color(CGColor(red: 1, green: 0, blue: 0, alpha: 1))
-            Text("Hello")
+            Color(UIColor.green.cgColor)
+            MyColorText()
         }
     }
 }
+
+struct MyColorText: View {
+    var body: some View{
+        Text("test")
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
